55859d8a202426e1b192fe1c44e272abe6303439
Lucas Coutinho <lucxscout@gmail.com>
2023-06-30 12:57:37 -0300

    feat: validação inputs email, senha

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-const eyeBtn = document.querySelector('.lnr-eye');
-
 const showPass = () => {
+  const eyeBtn = document.querySelector('.lnr-eye');
   const inputPass = document.querySelector('#floatingInputPassword');
   eyeBtn.addEventListener('mousedown', () => {
     inputPass.type = 'text';
@@ -16,3 +15,31 @@ const showPass = () => {
 };
 
 showPass();
+
+const inputEmailIsValid = () => {
+  const inputEmail = document.querySelector('#floatingInputEmail');
+  if (inputEmail.value == '') {
+    inputEmail.classList.add('is-invalid');
+  }
+};
+
+const inputPassIsValid = () => {
+  const inputPass = document.querySelector('#floatingInputPassword');
+  const elementInput = document.querySelector('.input-pass');
+  const eye = document.querySelector('.lnr-eye');
+  if (inputPass.value == '') {
+    inputPass.classList.add('is-invalid');
+    elementInput.removeChild(eye);
+  }
+};
+
+const submitBtn = () => {
+  const sendBtn = document.querySelector('.submitBtn');
+  sendBtn.addEventListener('click', (element) => {
+    element.preventDefault();
+    inputEmailIsValid();
+    inputPassIsValid();
+  });
+};
+
+submitBtn();
